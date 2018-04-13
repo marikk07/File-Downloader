1eeb6cef8d6f6508ea553a0fd7b199dfeabac6b1
Maryan Pasichniak <marikk07@ukr.net>
2018-04-13 13:45:01 +0300

    Initial Commit

---
+++ Download Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Download Animation
+//
+//  Created by pasichniak maryan on 4/13/18.
+//  Copyright © 2018 pasichniak maryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
